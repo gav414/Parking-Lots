9e8e62cac9bcd8d15c57e4e6e973f8b9230d1d22
PJ-Oscheh <phillip236.po@gmail.com>
2022-11-22 14:39:08 -0500

    start it lol

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
 # Main Python Code
+
+class ParkingLot:
+    def __init__(self, row, col, amount):
+        self.row = row
+        self.col = col
+        self.amount = amount
+        
+    def makeArray(self):
+        lotCol = []
+        lotRow = []
+        
+        for i in range(self.row):
+            for j in range(self.col):
+                lotCol.append('X')
+                print(f"col in j loop={lotCol}")
+            lotRow.append(lotCol)
+            print(f"row after append={lotRow}")
+            lotCol.clear()
+            print(f"col after j loop before i loop={lotCol}")
+            
+        print(lotRow)
+    
+    def selectSpot(self):
+        spot = str(input("Please select a spot: "))
+        spotRow = spot[0]
+        spotCol = spot[1]
+        print(f"spotRow={spotRow} and spotCol={spotCol}")
+        #isTaken(spotRow, spotCol)
+        
+    def isTaken(self, spotRow, spotCol):
+        pass
+
+coolLot = ParkingLot(9, 8, 10)
+
+coolLot.selectSpot()
