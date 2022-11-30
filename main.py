0a5e192f1fb728795bc4bb28a868e65465d0c0f7
gav414 <gav41403@gmail.com>
2022-11-30 18:36:05 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class ParkingLot:
         The number of rows the parking lot should have
     col: int
         The number of columns the parking lot should have
-    name: int
+    name: str
         The name of the parking lot.
     
     Methods
@@ -47,8 +47,11 @@ class ParkingLot:
         self.makeLotList()
         
     def __str__(self):
-        strLot = "  "
-        
+        strLot = "\n"
+        strLot += self.name + '\n'
+        for i in range(len(self.name)):
+            strLot += '-'
+        strLot += '\n  '
         #Write the column header based on how many columns we got
         for i in range(self.col):
             strLot += self.strTopRow[i] + " "
@@ -63,7 +66,7 @@ class ParkingLot:
                     
         return strLot
     
-    enableDebug = True
+    enableDebug = False
     def debugPrint(self, thing):
         '''
         Prints a debug print-out, if enableDebug is True.
@@ -105,11 +108,11 @@ class ParkingLot:
         spot = str(input("Please select a spot: "))
         spotCol = spot[0]
         spotRow = spot[1]
-        print(f"spotCol={spotCol} and spotRow={spotRow}")
-        #print(self.isTaken(spotRow, spotCol))
         if mode == "SELECT":
             if not (self.isTaken(spotCol, spotRow)):
                 self.lotList[int(spotRow)-1][self.getColInt(spotCol)] = "X"
+            else:
+                print("Cannot park in this spot")
         if mode == "UNSELECT":
             if (self.isTaken(spotCol, spotRow)):
                 self.lotList[int(spotRow)-1][self.getColInt(spotCol)] = "O"
@@ -131,6 +134,12 @@ class ParkingLot:
             if self.strTopRow[i] == spotCol:
                 colIndex = i
                 return colIndex
+
+    def doesPositionExist(self, spotCol, spotRow):
+        if ((self.getColInt(spotCol)+1 <= self.col) and (int(spotRow) <= self.row)):
+            return True
+        else:
+            return False
                 
     def isTaken(self, spotCol, spotRow): 
         '''
@@ -146,7 +155,7 @@ class ParkingLot:
                 False if spot is considered taken or unavailable
         '''
         self.debugPrint(f"spotCol={self.getColInt(spotCol)} and self.row={self.row}")
-        if (self.getColInt(spotCol)+1 <= self.col):
+        if self.doesPositionExist(spotCol, spotRow):
             colIndex = self.getColInt(spotCol)
             if self.lotList[int(spotRow)-1][colIndex] == "O":
                 return False
@@ -156,7 +165,7 @@ class ParkingLot:
             print("ERROR: This spot doesn't exist. Please select another spot.")
             return True;
 
-    def editLot(self, spotCol, spotRow, spotType):
+    def editLot(self):
         '''
         Specify where certain elements are in a parking lot, including
         employee spots, non-spots, or roads.
@@ -164,8 +173,11 @@ class ParkingLot:
                 spotCol(String): Letter representing the desired coulumn
                 spotRow(int): Integer representing the desired row
         '''
-        if isTaken(spotCol, spotRow):
-            self.lotList[spotRow][spotCol] = spotType
+        spotCol = input("Which column would you like to edit? ")
+        spotRow = int(input("Which row would you like to edit? "))
+        spotType = input("Would you like to change the spot to be empty (O), taken (X), or for employees (E)? ")
+        if not self.isTaken(spotCol, spotRow):
+            self.lotList[spotRow - 1][self.getColInt(spotCol)] = spotType
         else:
             print("Invalid spot.")
 
@@ -176,6 +188,11 @@ while True:
         row = int(input("How long would you like the parking lot? "))
         col = int(input("How many columns would you like your parking lot to have? "))
         lotName = input("What would you like to name the lot? ")
+        while True:
+            if lotName.lower() == "quit":
+                lotName = input("Not applicaple name, enter new name: ")
+            else:
+                break
         newLot = ParkingLot(row, col, lotName)
         pList.append(newLot)
     elif question.lower() == "no":
@@ -185,18 +202,26 @@ while True:
         print(pList)
     
 while True:
-    currentLotName = input("Which lot would you like to bring up? ")
+    currentLotName = input("Which lot would you like to bring up or would you like to quit? ")
+    if currentLotName.lower() == 'quit':
+        break
     for i in range(0, len(pList)):
         if currentLotName == pList[i].name:
-            currentLot = pList[i]
-            print(currentLot)
-            answer = input("Select spot \nUnselect spot \nQuit \nWhat would you like to do? ")
-            if answer.lower() == "select spot":
-                currentLot.selectSpot("SELECT")
-            elif answer.lower() == "unselect spot":
-                currentLot.selectSpot("UNSELECT")
-            elif answer.lower() == "quit":
-                break
-        print("Not an acceptable answer")
-    print("Not a name for a parking lot")
+            while True:
+                currentLot = pList[i]
+                print(currentLot)
+                answer = input("Park in spot \nLeave spot \nEdit Lot \nQuit \nWhat would you like to do? ")
+                if answer.lower() == "park in spot":
+                    currentLot.selectSpot("SELECT")
+                elif answer.lower() == "leave spot":
+                    currentLot.selectSpot("UNSELECT")
+                elif answer.lower() == "edit lot":
+                    currentLot.editLot()
+                elif answer.lower() == "quit":
+                    break
+                else:
+                    print("Not an acceptable answer")
+        elif currentLotName.lower() == "quit":
+            break
+    print("Not a name for a parking lot or 'quit' option.")
 
