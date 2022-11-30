2bf86836259538801c4891c04a1163fb7a333b93
PJ Oschmann <phillip236.po@gmail.com>
2022-11-29 23:06:59 -0500

    Add more docstrings, fix mixed up rows/cols
    More docstrings to hopefully make the code readable

--- main.py
+++ main.py
@@ -1,7 +1,35 @@
 # Main Python Code
 
 class ParkingLot:
+    '''
+    This class represents a Parking Lot, particularly one that can be found
+    at Baldwin Wallace.
     
+    Attributes
+    ----------
+    row: int
+        The number of rows the parking lot should have
+    col: int
+        The number of columns the parking lot should have
+    amount: int
+        I don't know either. TODO: Wtf is amount lmaoooo
+    
+    Methods
+    -------
+    debugPrint(thing):
+        Prints a debug print-out, if enableDebug is True.
+    makeLotList():
+        Generate a 2D List that serves as a visual representation of the
+        Parking Lot object.
+    selectSpot(mode):
+        Prompts the user to select a parking spot based on the lot's visual 
+        representation.
+    getColInt(self, spotCol):
+        Gets the integer value of the column based on the letter passed into the
+        program.
+    isTaken(self, spotCol, spotRow):
+        Determines whether a given spot, as passed in, is available to take.
+    '''
     
     def __init__(self, row, col, amount):
         self.row = row
@@ -47,7 +75,8 @@ class ParkingLot:
     
     def makeLotList(self):
         '''
-        Generate an 2D List that serves as a visual representation of the Parking Lot object.
+        Generate a 2D List that serves as a visual representation of the
+        Parking Lot object.
         Used internally.
         '''
         #Add desired number of rows
@@ -63,29 +92,60 @@ class ParkingLot:
     
     def selectSpot(self,mode):
         '''
-        Prompts the user to select a parking spot based on the lot's visual representation. Breaks
-        the input into a char (index 0) and integer (index 1).
+        Prompts the user to select a parking spot based on the lot's visual 
+        representation. Breaks the input into a char (index 0) and integer
+        (index 1).
+            Parameters:
+                mode ("String"): If mode is "SELECT," the user is trying to
+                    select a spot that is open. If mode is "UNSELECT," the
+                    user is trying to mark a spot that is taken as available.
         '''
         spot = str(input("Please select a spot: "))
-        spotRow = spot[0]
-        spotCol = spot[1]
-        print(f"spotRow={spotRow} and spotCol={spotCol}")
+        spotCol = spot[0]
+        spotRow = spot[1]
+        print(f"spotCol={spotCol} and spotRow={spotRow}")
         #print(self.isTaken(spotRow, spotCol))
-        if (self.isTaken(spotRow, spotCol)):
+        if (self.isTaken(spotCol, spotRow)):
             if mode == "SELECT":
-                self.lotList[int(spotCol)-1][self.getRowInt(spotRow)] = "X"
-            
+                self.lotList[int(spotRow)-1][self.getColInt(spotCol)] = "X"
+            #TODO: Implement mode UNSELECT (see docstring)
         
-    def getRowInt(self, spotRow):
+    def getColInt(self, spotCol):
+        '''
+        Gets the integer value of the column based on the letter passed into the
+        program.
+            Parameters:
+                spotCol (String): Letter representing the desired column
+            Returns:
+                colIndex (int): Integer that the column letter represents
+        '''
+        
+        #TODO: What happens if a lot is larger than 26?
+        #The program supports it. We just need characters to represent it.
         for i in range(len(self.strTopRow)):
-            if self.strTopRow[i] == spotRow:
-                rowIndex = i
-                return rowIndex
-    def isTaken(self, spotRow, spotCol):
-        self.debugPrint(f"spotRow={self.getRowInt(spotRow)} and self.col={self.col}")
-        if (self.getRowInt(spotRow)+1 <= self.col):
-            rowIndex = self.getRowInt(spotRow)
-            if self.lotList[int(spotCol)-1][rowIndex] == "O":
+            if self.strTopRow[i] == spotCol:
+                colIndex = i
+                return colIndex
+                
+    #TODO: I think the wording suggests the opposite return statement
+    #   I'm too tired to think about it tho lmao
+    def isTaken(self, spotCol, spotRow): 
+        '''
+        Determines whether a given spot, as passed in, is available to take.
+        If the spot is available, the function returns True.
+        If the spot is taken (or is an Employee spot, or not a spot), the
+        function returns False.
+            Parameters:
+                spotCol (String): Letter representing the desired column
+                spotRow (int): Integer representing the desired row
+            Returns:
+                True if spot is considered open
+                False if spot is considered taken or unavailable
+        '''
+        self.debugPrint(f"spotCol={self.getColInt(spotCol)} and self.row={self.row}")
+        if (self.getColInt(spotCol)+1 <= self.col):
+            colIndex = self.getColInt(spotCol)
+            if self.lotList[int(spotRow)-1][colIndex] == "O":
                 return True
             else:
                 return False
